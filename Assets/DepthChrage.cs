7e22894bd471527ab6d6d46742a6177efd2236be
Michal Sodomka <m.sodomka@equitania.com>
2016-02-13 14:01:47 +0100

    Inital commit

---
+++ Assets/DepthChrage.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DepthChrage : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        //Check the provided Collider2D parameter other to see if it is tagged "PickUp", if it is...
+        if (other.gameObject.CompareTag("Bottom"))
+        {
+            Debug.Log("Colided with gorund");
+            //... then set the other object we just collided with to inactive.
+            other.gameObject.SetActive(false); 
+                       
+        }
+    }
+}
---
+++ Assets/Sea.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Sea : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        SpriteRenderer sr = GetComponent<SpriteRenderer>();
+        float worldScreenHeight = Camera.main.orthographicSize * 2;
+        float worldScreenWidth = worldScreenHeight / Screen.height * Screen.width;
+
+        // set height and width to same value as actual camera
+        transform.localScale = new Vector3(worldScreenWidth / sr.sprite.bounds.size.x, 1, 1);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
